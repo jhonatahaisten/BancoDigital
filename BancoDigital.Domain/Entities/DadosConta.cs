6796d56ae925a9dc339487b32532a33c9094c13b
DESKTOP-CPUQFT0\Jhonata <jhonatahaisten@gmail.com>
2021-06-20 11:31:00 -0300

    Adicionar arquivos de projeto.

---
+++ BancoDigital.Domain/Entities/DadosConta.cs
@@ -0,0 +1,9 @@
+﻿namespace BancoDigital.Domain.Entities
+{
+    public class DadosConta
+    {
+        public string id { get; set; }
+        public int conta { get; set; }
+        public double saldo { get; set; }
+    }
+}
